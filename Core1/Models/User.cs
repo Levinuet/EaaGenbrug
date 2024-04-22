a827f7a8e4f987e8a2ca3c4bb8908783885f82c4
nicolaims <146664107+nicolaims@users.noreply.github.com>
2024-04-22 09:36:07 +0200

    Hej

---
+++ Core1/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models { 
+
+public class User
+    {
+        [Required, MinLength(3)]
+        public required string Username { get; set; }
+
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public required string Password { get; set; }
+    }
+
+};
