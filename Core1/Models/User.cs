919c52b28710c3cdf3e62cb88e2b24ee7ec0f72d
nicolaims <146664107+nicolaims@users.noreply.github.com>
2024-04-22 09:40:13 +0200

    123

--- Core1/Models/User.cs
+++ Core1/Models/User.cs
@@ -5,13 +5,13 @@ namespace Core.Models {
 public class User
     {
         [Required, MinLength(3)]
-        public required string Username { get; set; }
+        public string Username { get; set; }
 
         [Required, EmailAddress]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
         [Required, MinLength(6)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
     }
 
 };
