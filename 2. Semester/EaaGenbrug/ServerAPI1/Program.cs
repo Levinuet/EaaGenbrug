f5f1058ad45893698366885c4c26867c488d86c2
Levi Emkjær <eaa23leej@students.eaaa.dk>
2024-04-23 13:27:42 +0200

    Update UL to Main

---
+++ 2. Semester/EaaGenbrug/ServerAPI1/Program.cs
@@ -0,0 +1,51 @@
+using MongoDB.Driver;
+using ServerAPI.Repositories;
+
+
+namespace ServerAPI1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+           builder.Services.AddHttpClient("MyClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7052/");
+            });
+
+
+            // Add services to the container.
+            builder.Services.AddSingleton(sp => new MongoDbContext(
+                builder.Configuration["MongoDB:ConnectionString"],
+                builder.Configuration["MongoDB:DatabaseName"]
+            ));
+            builder.Services.AddScoped<UserRepository>();
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+            builder.Services.AddControllers();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseAuthorization();
+            app.MapControllers();
+            app.MapBlazorHub();
+
+            app.Run();
+
+        }
+
+        public class MongoDbContext
+        {
+            public IMongoDatabase Database { get; }
+
+            public MongoDbContext(string connectionString, string dbName)
+            {
+                var client = new MongoClient("mongodb+srv://nicolaischmidt59:Uct89stc@cluster0.nczpyyv.mongodb.net/");
+                Database = client.GetDatabase("Genbrugsmarked");
+            }
+        }
+    }
+}
