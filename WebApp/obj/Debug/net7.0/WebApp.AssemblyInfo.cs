dd96bceaff171bb215bd740ea6823f1c2e2160cb
Levi Emkjær <eaa23leej@students.eaaa.dk>
2024-04-24 10:17:07 +0200

    møs

--- WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c7d52446f2988b2da1edb483a4f3a3a9a249d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8e30b7127f2b3d30f830542066a73bda655ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
